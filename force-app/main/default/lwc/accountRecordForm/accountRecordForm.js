27bde94f1ab279764e2f2991a139960c5abbe2d7
Sarveswara rao <rao.sarveswara@gmail.com>
2020-06-24 15:46:49 +1000

    newly added

--- force-app/main/default/lwc/accountRecordForm/accountRecordForm.js
+++ force-app/main/default/lwc/accountRecordForm/accountRecordForm.js
@@ -1,10 +1,11 @@
-import { LightningElement,track } from 'lwc';
+import { LightningElement,track,api } from 'lwc';
 //import NAME_FIELD from '@salesforce/schema/Account.Name';
 //import PHN_FIELD from '@salesforce/schema/Account.Phone';
 //import WEBSITE_FIELD from '@salesforce/schema/Account.Website';
 
 export default class AccountRecordForm extends LightningElement {
-    @track recordId;
+    @api recordId;
+    @api objectApiName;
     //fieldArray=[NAME_FIELD,PHN_FIELD,WEBSITE_FIELD];
 
     handleSuccess(event){
