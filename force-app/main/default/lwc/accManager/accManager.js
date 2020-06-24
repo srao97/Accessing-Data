87b1066e6d5adfcbaa0ee48484d67cb43ab12460
Sarveswara rao <rao.sarveswara@gmail.com>
2020-06-24 12:27:21 +1000

    added component

--- force-app/main/default/lwc/accManager/accManager.js
+++ force-app/main/default/lwc/accManager/accManager.js
@@ -1,6 +1,6 @@
 import { LightningElement,wire,track } from 'lwc';
 import getAllAccounts from '@salesforce/apex/AccountManager.getAccount';
-
+import {ShowToastEvent} from 'lightning/platformShowToastEvent';
 export default class AccManager extends LightningElement {
     /*@wire(getAllAccounts)
     accounts;*/
@@ -21,8 +21,21 @@ export default class AccManager extends LightningElement {
     getAccounts(){
         getAllAccounts({numberOfRecords:this.numberOfRecords}).then(response=>{
             this.accounts=response;
+            const toastEvent=new ShowToastEvent({
+                title:'Account Loaded',
+                message:this.numberOfRecords +' Accounts Fetched from server',
+                variant : 'success',
+
+            });
+            this.dispatchEvent(toastEvent);
         }).catch(error=>{
             console.error('error in getting the accounts',error.body.message);
+            const toastEvent = new ShowToastEvent({
+                title : 'Error',
+                message : this.numberOfRecords + ' Please input the value',
+                variant : 'error',
+            });
+            this.dispatchEvent(toastEvent);
         })
     }
 }
